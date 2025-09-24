7c86c098fb766c629188ad5a9817ce38a2e58bb7
a1917 <1917659904@qq.com>
2025-09-24 23:51:58 +0800

    user test

--- example/callee/userservice.cc
+++ example/callee/userservice.cc
@@ -11,4 +11,23 @@ public:
         cout << "name: " << name << " pwd: " << pwd << endl;
         return true;
     }
+
+    void Login(::google::protobuf::RpcController* controller,
+                       const ::example::LoginRequest* request,
+                       ::example::LoginResponse* response,
+                       ::google::protobuf::Closure* done){
+        bool ret = Login(request->name(), request->pwd());
+        if(ret){
+            response->mutable_result()->set_code(200);
+            response->mutable_result()->set_msg("");
+        }else{
+            response->mutable_result()->set_code(400);
+            response->mutable_result()->set_msg("login failed");
+        }
+        done->Run();
+    }
+};
+
+int main(){
+    
 }
